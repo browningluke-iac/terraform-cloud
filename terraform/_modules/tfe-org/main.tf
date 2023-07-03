68284a197d9777037a0900b50a7f09a5e12bfd65
Luke Browning <browningluke01@gmail.com>
2023-07-03 03:51:20 -0700

    Use defaults from vars

--- terraform/_modules/tfe-org/main.tf
+++ terraform/_modules/tfe-org/main.tf
@@ -20,28 +20,28 @@ resource "tfe_workspace" "workspaces" {
 
   # Meta
   name        = each.key
-  description = lookup(each.value, "description", "")
-  project_id  = can(each.value.project) ? tfe_project.projects[each.value.project].id : null
-  tag_names   = concat(lookup(each.value, "tags", []), ["managed-in-terraform"])
+  description = each.value.description
+  project_id  = try(tfe_project.projects[each.value.project].id, null)
+  tag_names   = concat(each.value.tags, ["managed-in-terraform"])
 
   # Execution
-  execution_mode      = lookup(each.value, "execution_mode", "local")
-  speculative_enabled = lookup(each.value, "speculative_plans", true)
-  auto_apply          = lookup(each.value, "auto_apply", false)
+  execution_mode      = each.value.execution_mode
+  speculative_enabled = each.value.speculative_plans
+  auto_apply          = each.value.auto_apply
 
-  queue_all_runs = lookup(each.value, "queue_all_runs", true)
+  queue_all_runs = each.value.queue_all_runs
 
-  working_directory = can(each.value.vcs.working_dir) ? each.value.vcs.working_dir : null
+  working_directory = try(each.value.vcs.working_dir, null)
 
   trigger_patterns = try(each.value.vcs.trigger_patterns, null)
 
   # VCS
   dynamic "vcs_repo" {
-    for_each = can(each.value.vcs.repo) ? [each.value.vcs] : []
+    for_each = each.value.vcs != null ? [each.value.vcs] : []
 
     content {
       identifier     = vcs_repo.value.repo
-      branch         = lookup(vcs_repo.value, "branch", "")
+      branch         = vcs_repo.value.branch
       oauth_token_id = tfe_oauth_client.github.oauth_token_id
     }
   }
